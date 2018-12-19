6b54d63564b61ee1ac6835cfa7d3ff3648e199bc
Nikita Orekhov <nikanor97@gmail.com>
2018-12-19 18:48:47 +0300

    Update ProfileActivity.java
    add task_9 functionality

--- task3/app/src/main/java/com/example/nikan/task3/ProfileActivity.java
+++ task3/app/src/main/java/com/example/nikan/task3/ProfileActivity.java
@@ -3,12 +3,21 @@ package com.example.nikan.task3;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity
+        implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener{
+
+    private static final String DEBUG_TAG = "Results by Gestures";
+    private GestureDetectorCompat mDetector;
 
     private static final String ID_KEY = "ID_KEY";
 
@@ -30,7 +39,75 @@ public class ProfileActivity extends AppCompatActivity {
         final ImageView imageView = findViewById(R.id.imageView);
         textView.setText(person.getName());
         imageView.setImageResource(R.drawable.face_small);
+
+        mDetector = new GestureDetectorCompat(this, this);
+        mDetector.setOnDoubleTapListener(this);
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (this.mDetector.onTouchEvent(event)) {
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDown: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onFling(
+            MotionEvent event1, MotionEvent event2,
+            float velocityX, float velocityY) {
+        Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onLongPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onScroll(
+            MotionEvent event1, MotionEvent event2, float distanceX,
+            float distanceY) {
+        Intent intent = new Intent(this, RecyclerViewActivity.class);
+        startActivity(intent);
+        Log.d(DEBUG_TAG, "onScroll: " + event1.toString() + event2.toString());
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onShowPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapUp: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDoubleTap: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDoubleTapEvent: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + event.toString());
+        return true;
+    }
 
 }
